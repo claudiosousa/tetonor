43075c980e03b1db53130824dbac2d5af2605da0
Claudio <claudio.sousa@gmail.com>
2018-11-17 21:06:13 +0100

    layouting grid

--- src/webapp/components/tetonor.js
+++ src/webapp/components/tetonor.js
@@ -1,7 +1,64 @@
+const NEXT_OPERATOR = {
+    '': '+',
+    '+': 'x',
+    x: ''
+};
+
 export default {
     data: function() {
         return {
+            choices: [
+                ...[
+                    1,
+                    2,
+                    null,
+                    4,
+                    4,
+                    5,
+                    6,
+                    6,
+                    6,
+                    9,
+                    10,
+                    10,
+                    13,
+                    13,
+                    16,
+                    16
+                ].map(v => ({
+                    val: v
+                }))
+            ],
+            problems: [
+                ...[
+                    14,
+                    15,
+                    16,
+                    18,
+                    24,
+                    17,
+                    17,
+                    52,
+                    10,
+                    32,
+                    20,
+                    12,
+                    26,
+                    45,
+                    100,
+                    36
+                ].map(r => ({
+                    res: r,
+                    inputA: null,
+                    operator: '',
+                    inputB: null
+                }))
+            ],
             msg: 'TETONOR GAME'
         };
+    },
+    methods: {
+        toggleOperator: problem =>
+            (problem.operator = NEXT_OPERATOR[problem.operator])
     }
 };
